228372bfa7097583bcb87846f0abdb4f33f6ea49
linyihaolyh <35909212+linyihaolyh@users.noreply.github.com>
2022-10-17 16:30:41 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -10,20 +10,18 @@ import torch.nn.functional as F
 
 
 class SRCNN(nn.Module):
-    def __init__(self,csize):
+    def __init__(self,csize=1):
         super(SRCNN,self).__init__()
-        self.lrelu=nn.LeakyRelu()
-        self.conv1=nn.Conv2d(csize,64,kernal_size=9,padding=9//2)
-        self.conv2=nn.Conv2d(64,32,kernal_size=7,padding=7//2)
-        self.conv3=nn.Conv2d(32,32,kernal_size=5,padding=5//2)
-        self.conv4=nn.Conv2d(32,csize,kernal_size=5,padding=5//2)
+        self.lrelu=F.leaky_relu
+        self.conv1=nn.Conv2d(csize,64,kernel_size=9,padding=0)
+        self.conv2=nn.Conv2d(64,32,kernel_size=1,padding=0)
+        self.conv3=nn.Conv2d(32,csize,kernel_size=5,padding=0)
         
         
         
     def forward(self,x):
         x=self.lrelu(self.conv1(x))
         x=self.lrelu(self.conv2(x))
-        x=self.lrelu(self.conv3(x))
-        x=self.conv4(x)
+        x=self.conv3(x)
         
         return x
